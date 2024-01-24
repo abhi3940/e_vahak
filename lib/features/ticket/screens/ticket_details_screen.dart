4cdff6739a1049e6aff14f09affe3bd5232a67ec
SOHAMSABLE <soham.s.sable8@gmail.com>
2024-01-24 11:31:07 +0530

    Changes to be committed: 	modified:   lib/features/ticket/screens/ticket_details_screen.dart
    Changes not staged for commit:
    	modified:   lib/features/home/screens/home_screen.dart

---
+++ lib/features/ticket/screens/ticket_details_screen.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:e_vahak/theme/pallete.dart';
+
+
+class Ticket extends StatelessWidget {
+  const Ticket({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Stack(
+        children: [
+          const Column(
+            children: [
+              Padding(
+                padding:  EdgeInsets.all(16),
+                child: Center(
+                  child: Text(
+                    "Ticket Details",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                  ),
+                ),
+              ),
+              SizedBox(height: 16),
+            ],
+          ),
+          Positioned(
+            top: 252,
+            left: 28,
+            child: Card(
+              margin: const EdgeInsets.all(0), 
+              color: Pallete.grey, 
+              elevation: 0, 
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Container(
+                width: 323,
+                height: 240,
+                child: const Padding(
+                  padding:  EdgeInsets.all(16),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text("No: 0000956          06-10-24            14:58:47"),
+                      SizedBox(height: 24), 
+                      Center(child: Text("Stop A     To     Stop B")),
+                      SizedBox(height: 24),
+                      Text("Full : 1  X  20.00 = Rs. 20.00"),
+                      Text("Booked by: User A"),
+                      SizedBox(height: 24),
+                      Center(child: Text("Rs. 20.00")),
+                      SizedBox(height: 24),
+
+                      Center(child: Text("NOT TRANSFERABLE")),
+                      Center(child: Text("HELPLINE : 1000-88-6969")),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: 16,
+            left: 16,
+            right: 16,
+            child: ElevatedButton(
+              onPressed: () {
+                // Add your logic for the "Done" button
+              },
+              style: ElevatedButton.styleFrom(
+                primary: Color.fromRGBO(15, 163, 210, 1),
+                onPrimary: Colors.white,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                minimumSize: Size(double.infinity, 50),
+              ),
+              child: Text("Done"),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
